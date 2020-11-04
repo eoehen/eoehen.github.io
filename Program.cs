9c1b41487235a9b1eb0e31c617bde190978e70cc
Elias Oehen <oee@bbtsoftware.ch>
2020-11-04 10:22:42 +0100

    Add statiq.web bootstrap

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace eoehen.github.io
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToGitHubPages(
+                    "statiqdev",
+                    "statiqdev.github.io",
+                    Config.FromSetting<string>("GITHUB_TOKEN"))
+                .RunAsync();
+    }
+}
